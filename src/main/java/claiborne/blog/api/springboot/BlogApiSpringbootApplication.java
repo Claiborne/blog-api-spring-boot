c48e1673924a20dc6b195d8b3c8026f6ac485b2e
Will <willclaiborne@gmail.com>
2022-07-29 02:07:11 -0400

    first commit

---
+++ src/main/java/claiborne/blog/api/springboot/BlogApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package claiborne.blog.api.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/claiborne/blog/api/springboot/BlogApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package claiborne.blog.api.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
