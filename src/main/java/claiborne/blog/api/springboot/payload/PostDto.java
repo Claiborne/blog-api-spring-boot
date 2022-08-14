a19ee92d0d5e30fa58081d11d675fd98dea498a0
Will <willclaiborne@gmail.com>
2022-08-14 02:42:48 -0400

    add validation annotations to post dto

--- src/main/java/claiborne/blog/api/springboot/payload/PostDto.java
+++ src/main/java/claiborne/blog/api/springboot/payload/PostDto.java
@@ -2,11 +2,23 @@ package claiborne.blog.api.springboot.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class PostDto {
 
   private long id;
+
+  @NotEmpty
+  @Size(min = 2, message = "Title should have at least 2 characters")
   private String title;
+
+  @NotEmpty
+  @Size(min = 5, message = "Description should have at least 5 characters")
   private String description;
+
+  @NotEmpty
+  @Size(min = 5, message = "Content should have at least 5 characters")
   private String content;
 }
