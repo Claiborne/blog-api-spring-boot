ea2a1c4f1f4372ad2dd2dfff6bc3495acf973aed
Will <willclaiborne@gmail.com>
2022-08-15 08:01:42 -0400

    add swagger

--- src/main/java/claiborne/blog/api/springboot/controller/PostController.java
+++ src/main/java/claiborne/blog/api/springboot/controller/PostController.java
@@ -4,12 +4,15 @@ import claiborne.blog.api.springboot.payload.PostDto;
 import claiborne.blog.api.springboot.payload.PostResponse;
 import claiborne.blog.api.springboot.service.PostService;
 import claiborne.blog.api.springboot.utils.AppConfigs;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(value = "API for Blog Posts")
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
@@ -22,6 +25,7 @@ public class PostController {
     this.postService = postService;
   }
 
+  @ApiOperation(value = "Create a blog post")
   //@PreAuthorize("hasRole('ROLE_ADMIN')")
   // @PostMapping impl has: @RequestMapping(method=RequestMethod.POST)
   @PostMapping
@@ -29,6 +33,7 @@ public class PostController {
     return new ResponseEntity<>(postService.create(postDto), HttpStatus.CREATED);
   }
 
+  @ApiOperation(value = "Get all blog posts")
   @GetMapping
   public PostResponse getAllPosts(
       @RequestParam(value = "page", defaultValue = AppConfigs.DEFAULT_PAGE_NUMBER, required = false) int page,
@@ -39,16 +44,19 @@ public class PostController {
     return postService.getAll(page, count, sort, order);
   }
 
+  @ApiOperation(value = "Get a blog post by id")
   @GetMapping("/{id}")
   public ResponseEntity<PostDto> getById(@PathVariable(name = "id") long id) {
     return ResponseEntity.ok(postService.getById(id));
   }
 
+  @ApiOperation(value = "Update a blog post")
   @PutMapping("/{id}")
   public ResponseEntity<PostDto> update(@Valid @RequestBody PostDto dto, @PathVariable(name = "id") long id) {
     return new ResponseEntity<>(postService.update(dto, id), HttpStatus.OK);
   }
 
+  @ApiOperation(value = "Delete a blog post")
   @DeleteMapping("/{id}")
   public ResponseEntity<String> delete(@PathVariable(name = "id") long id) {
     postService.delete(id);
