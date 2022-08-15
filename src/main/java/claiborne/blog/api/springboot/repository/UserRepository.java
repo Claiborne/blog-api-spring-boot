7ba4773f2d412916f0a1b34e9235a7cf332d860f
Will Claiborne <willclaiborne@gmail.com>
2022-08-14 20:16:36 -0400

    rollback security changes

---
+++ src/main/java/claiborne/blog/api/springboot/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package claiborne.blog.api.springboot.repository;
+
+import claiborne.blog.api.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
